40ce41e0fd902649517f753f64a36b3745236b41
Anatolii Voloshyn <a.voloshin.p@gmail.com>
2023-04-04 13:17:04 +0300

    first commit

---
+++ src/components/YouTubeButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <a 
+    :href="href"
+    target="_blank"
+    class="
+      px-3
+      py-2
+      rounded
+      border-2
+      border-red-500
+      text-red-500
+      hover:bg-red-500
+      hover:text-white
+      hover:border-red-700
+      font-semibold"
+  >
+    <slot></slot>
+  </a>
+</template>
+
+<script setup>
+const props = defineProps({
+  href: String
+})
+</script>
---
+++ src/store/state.js
@@ -0,0 +1,6 @@
+export default {
+  searchedMeals: [],
+  MealsByLetter: [],
+  MealsByIngredient: [],
+  ingredient: {},
+}
